6b33edf9d5a14c72f4001154cbc25e9e3ac4cc7f
Igor Korobeynikov <igrock88@gmail.com>
2025-05-22 16:54:18 +0800

    remove

--- src/main/java/ru/onlineService/telegramBot/Commands.java
+++ src/main/java/ru/onlineService/telegramBot/Commands.java
@@ -1,12 +1,9 @@
 package ru.onlineService.telegramBot;
 
-import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.request.*;
 import com.pengrad.telegrambot.request.SendMessage;
 
-import com.pengrad.telegrambot.response.SendResponse;
 import ru.onlineService.telegramBot.chatData.Chat;
 import ru.onlineService.telegramBot.chatData.ChatHandler;
 import ru.onlineService.telegramBot.commands.*;
@@ -32,60 +29,11 @@ public class Commands {
             return;
         }
         String chatId = String.valueOf(update.message().chat().id());
-        if (update.message().text().startsWith("/menu")){
 
-            Keyboard keyboard = new ReplyKeyboardMarkup(
-                    new KeyboardButton[]{
-                            new KeyboardButton("Предоставить телефон").requestContact(true).requestPoll(new KeyboardButtonPollType()),
-                    }
-            );
-
-            SendMessage request = new SendMessage(chatId, "Предоставьте телефон")
-                    .replyMarkup(keyboard);
-            bot.execute(request, new Callback<SendMessage, SendResponse>() {
-                @Override
-                public void onResponse(SendMessage request, SendResponse response) {
-                    System.out.println(response.message().replyMarkup());
-                }
-
-                @Override
-                public void onFailure(SendMessage sendMessage, IOException e) {
-                    System.out.println(sendMessage);
-                }
-
-
-
-            });
-
-
-        }
         if (update.message().text().startsWith("/start") || update.message().text().startsWith("/help")) {
-//            bot.execute(new SendMessage(chatId, "Здравствуйте. Вас привествует бот для сотрудников компании Ночной Экспресс. Для подписки на сообщения необходимо авторизоваться\n\n" +
-//                    "Список комманд: \n" + AUTH + " [пароль] - без квадратных скобок. Авторизация\n" +
-//                    EXIT + " - выход  "));
-
-
-            Keyboard keyboard = new ReplyKeyboardMarkup(
-                    new KeyboardButton[]{
-                            new KeyboardButton("Предоставить телефон").requestContact(true).requestPoll(new KeyboardButtonPollType()),
-                    }
-            );
-
-            SendMessage request = new SendMessage(chatId, "Предоставьте телефон")
-                    .replyMarkup(keyboard);
-            bot.execute(request, new Callback<SendMessage, SendResponse>() {
-                @Override
-                public void onResponse(SendMessage request, SendResponse response) {
-                    System.out.println(response.message().leftChatMember().toString());
-                }
-
-                @Override
-                public void onFailure(SendMessage sendMessage, IOException e) {
-                    System.out.println(sendMessage);
-                }
-
-
-            });
+            bot.execute(new SendMessage(chatId, "Здравствуйте. Для подписки на сообщения необходимо авторизоваться\n\n" +
+                    "Список комманд: \n" + AUTH + " [пароль] - без квадратных скобок. Авторизация\n" +
+                    EXIT + " - выход  "));
             return;
         }
 
